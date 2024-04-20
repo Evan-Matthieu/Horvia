62e27d35d5d8858334ceb534115a08250c635f35
FoxySnakes <matthox45@gmail.com>
2024-04-20 18:32:38 +0200

    add profile ui

---
+++ App/app/src/main/java/com/horvia/horvia/ui/profile/LastOrdersFragment.java
@@ -0,0 +1,33 @@
+package com.horvia.horvia.ui.profile;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.horvia.horvia.R;
+
+public class LastOrdersFragment extends Fragment {
+
+
+    public LastOrdersFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_last_orders, container, false);
+        return view;
+    }
+}
